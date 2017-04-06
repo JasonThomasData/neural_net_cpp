7d6bf0497ade5a50c3ba4b5f6880740c51ea6d6e
john smith <jason.robert.thomas.123@gmail.com>
2017-04-06 13:39:24 +1000

    Moved existing tests into separate files, so they're more easily managed.

--- makefile
+++ makefile
@@ -1,8 +1,8 @@
-all: synapse.o soma.o input_neuron.o neuron.o neural_net.o neural_net integration.o tests clean
+all: synapse.o soma.o input_neuron.o neuron.o network.o main.o classifier.o main int_tests_synapse.o int_tests_soma.o int_tests_neuron.o int_tests_main.o integration clean
 .PHONY: all CXXFLAGS clean
 CXXFLAGS = -std=c++14 -Wall
 
-
+#Network stuff
 synapse.o: src/network/synapse/synapse.cpp
 	g++ -c $(CXXFLAGS) src/network/synapse/synapse.cpp -o build/synapse.o
 soma.o: src/network/soma/soma.cpp
@@ -11,16 +11,30 @@ input_neuron.o: src/network/input_neuron/input_neuron.cpp
 	g++ -c $(CXXFLAGS) src/network/input_neuron/input_neuron.cpp -o build/input_neuron.o
 neuron.o: src/network/neuron/neuron.cpp
 	g++ -c $(CXXFLAGS) src/network/neuron/neuron.cpp -o build/neuron.o
-neural_net.o: src/exe/neural_net.cpp
-	g++ -c $(CXXFLAGS) src/exe/neural_net.cpp -o build/neural_net.o
-neural_net:
-	g++ $(CXXFLAGS) build/synapse.o build/soma.o build/input_neuron.o build/neuron.o build/neural_net.o -o bin/neural_net
+network.o: src/network/network.cpp
+	g++ -c $(CXXFLAGS) src/network/network.cpp -o build/network.o
 
 
-integration.o:
-	g++ -c $(CXXFLAGS) test/integration.cpp -o build/integration.o
-tests:
-	g++ $(CXXFLAGS) build/integration.o build/soma.o build/synapse.o build/input_neuron.o build/neuron.o -o bin/tests/integration
+classifier.o: src/classifier/classifier.cpp
+	g++ -c $(CXXFLAGS) src/classifier/classifier.cpp -o build/classifier.o
+
+
+main.o: src/exe/main.cpp
+	g++ -c $(CXXFLAGS) src/exe/main.cpp -o build/main.o
+main:
+	g++ $(CXXFLAGS) build/synapse.o build/soma.o build/input_neuron.o build/neuron.o build/network.o build/main.o -o bin/main
+
+#Integration tests
+int_tests_synapse.o: test/integration/synapse.cpp
+	g++ -c $(CXXFLAGS) test/integration/synapse.cpp -o build/int_tests_synapse.o
+int_tests_soma.o: test/integration/soma.cpp
+	g++ -c $(CXXFLAGS) test/integration/soma.cpp -o build/int_tests_soma.o
+int_tests_neuron.o: test/integration/neuron.cpp
+	g++ -c $(CXXFLAGS) test/integration/neuron.cpp -o build/int_tests_neuron.o
+int_tests_main.o: test/integration/main.cpp
+	g++ -c $(CXXFLAGS) test/integration/main.cpp -o build/int_tests_main.o
+integration:
+	g++ $(CXXFLAGS) build/int_tests_synapse.o build/int_tests_soma.o build/int_tests_neuron.o build/int_tests_main.o build/soma.o build/synapse.o build/input_neuron.o build/neuron.o build/network.o build/classifier.o -o bin/tests/integration
 
 
 clean:
---
+++ test/integration/neuron.cpp
@@ -0,0 +1,83 @@
+#include "../catch.h"
+#include "../../src/network/input_neuron/input_neuron.h"
+#include "../../src/network/synapse/synapse.h"
+#include "../../src/network/neuron/neuron.h"
+
+TEST_CASE( "neuron - activate, via one incoming synapse #1 ") {
+    /* For a neuron to activate, the synapses coming in must all have weights, and the neurons those
+     * synapses originate from must have an output_value
+     */
+
+    InputNeuron neuron1;
+    neuron1.outgoing_value = 1.0;
+
+    Neuron neuron2;
+
+    Synapse new_synapse(neuron1, neuron2);
+    new_synapse.weight = 0.8;
+    neuron2.add_incoming_synapse(new_synapse);
+
+    /* Incoming weights are equal to 1.0*0.8 = 0.8 */
+    neuron2.set_in_out_values();
+
+    float expected_result = 0.689974;
+    float actual_result = neuron2.get_outgoing_value();
+    REQUIRE(Approx(actual_result) == expected_result);
+}
+
+TEST_CASE( "neuron - activate, via one incoming synapse #2 ") {
+    /* For a neuron to activate, the synapses coming in must all have weights, and the neurons those
+     * synapses originate from must have an output_value
+     */
+
+    InputNeuron neuron1;
+    neuron1.outgoing_value = 0.8;
+
+    Neuron neuron2;
+
+    Synapse new_synapse(neuron1, neuron2);
+    new_synapse.weight = 0.5;
+    neuron2.add_incoming_synapse(new_synapse);
+
+    /* Incoming weights are equal to 0.5*0.8 = 0.4*/
+    neuron2.set_in_out_values();
+
+    float expected_result = 0.598688;
+    float actual_result = neuron2.get_outgoing_value();
+    REQUIRE(Approx(actual_result) == expected_result);
+}
+
+TEST_CASE( "neuron - activate, via several incoming synapses ") {
+    /* For a neuron to activate, the synapses coming in must all have weights, and the neurons those
+     * synapses originate from must have an output_value
+     */
+
+    InputNeuron neuron1;
+    neuron1.outgoing_value = 0.4;
+
+    InputNeuron neuron2;
+    neuron2.outgoing_value = 1.0;
+
+    InputNeuron neuron3;
+    neuron3.outgoing_value = 0.3;
+
+    Neuron neuron4;
+
+    Synapse new_synapse_1(neuron1, neuron4);
+    new_synapse_1.weight = 0.75;
+    Synapse new_synapse_2(neuron2, neuron4);
+    new_synapse_2.weight = 0.2;
+    Synapse new_synapse_3(neuron3, neuron4);
+    new_synapse_3.weight = 0.5;
+
+    neuron4.add_incoming_synapse(new_synapse_1);
+    neuron4.add_incoming_synapse(new_synapse_2);
+    neuron4.add_incoming_synapse(new_synapse_3);
+
+    neuron4.set_in_out_values();
+
+    /* Incoming values should be = 0.4*0.75 + 1.0*0.2 + 0.3*0.5 = 0.65 */
+    float expected_result = 0.65701;
+    float actual_result = neuron4.get_outgoing_value();
+    REQUIRE(Approx(actual_result) == expected_result);
+}
