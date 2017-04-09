b349111e6d9e25fa3ca2f9c04916b89967faa799
john smith <jason.robert.thomas.123@gmail.com>
2017-04-09 22:37:24 +1000

    Moving file, so eventually unit tests can have the same file.

--- test/integration/main.cpp
+++
@@ -1,8 +0,0 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
-#include "../catch.h"
-#include "../../src/network/neuron/input.h"
-#include "../../src/network/synapse/synapse.h"
-#include "../../src/network/neuron/neuron.h"
-#include "../../src/network/soma/soma.h"
-#include "../../src/network/network.h"
-
