2f8fdfb7a74a8ad770c10e84fdeef1ee41cb3052
john smith <jason.robert.thomas.123@gmail.com>
2017-05-03 12:03:18 +1000

    New tests to check the reader and parser behave as expected.

---
+++ test/src/parser.cpp
@@ -0,0 +1,63 @@
+#include "../catch.h"
+#include "../../src/reader/parser.h"
+#include <vector>
+#include <iostream>
+
+TEST_CASE( "parser - unit test - test a string is parsed to a vector")
+{
+    Parser parser;
+    std::string str_to_parse = "2,2,1";
+
+    std::vector<std::string> parsed_vector = parser.string_vector(str_to_parse);
+
+    int expected_result_1 = 3;
+    int actual_result_1 = parsed_vector.size();
+    REQUIRE(actual_result_1 == expected_result_1);
+
+    std::string expected_result_2 = "2";
+    std::string actual_result_2 = parsed_vector.at(1);
+    REQUIRE(actual_result_2 == expected_result_2);
+}
+
+TEST_CASE( "parser - unit test - vector of strings is parsed to a vector of ints")
+{
+    Parser parser;
+    std::vector<std::string> vector_to_parse = {"4","10","4"};
+
+    std::vector<int> parsed_vector = parser.string_vector_int_vector(vector_to_parse);
+
+    int expected_result_1 = 3;
+    int actual_result_1 = parsed_vector.size();
+    REQUIRE(actual_result_1 == expected_result_1);
+
+    int expected_result_2 = 4;
+    int actual_result_2 = parsed_vector.at(2);
+    REQUIRE(actual_result_2 == expected_result_2);
+}
+
+TEST_CASE( "parser - unit test - vector of strings is parsed to a vector of doubles")
+{
+    Parser parser;
+    std::vector<std::string> vector_to_parse = {"3","2","2","2"};
+
+    std::vector<double> parsed_vector = parser.string_vector_double_vector(vector_to_parse);
+
+    int expected_result_1 = 4;
+    int actual_result_1 = parsed_vector.size();
+    REQUIRE(actual_result_1 == expected_result_1);
+
+    double expected_result_2 = 2.0;
+    double actual_result_2 = parsed_vector.at(3);
+    REQUIRE(actual_result_2 == expected_result_2);
+}
+
+TEST_CASE( "parser - unit test - string to int, double, throws error")
+{
+    Parser parser;
+    std::string int_string = "A.";
+    std::string double_string = "@";
+
+    REQUIRE_THROWS( parser.to_int(int_string) );
+    REQUIRE_THROWS( parser.to_double(double_string) );
+}
+
---
+++ test/src/reader.cpp
@@ -0,0 +1,52 @@
+#include "../catch.h"
+#include "../../src/reader/reader.h"
+#include "../../src/reader/parsed_data.h"
+
+TEST_CASE( "reader - integration test - test there's no file")
+{
+    Reader reader;
+
+    std::string file_name = "totally not a real file";
+
+    REQUIRE_THROWS( reader.read_training_data(file_name) );
+}
+
+/*
+ * This test uses test/training_data file, which is a Nand gate
+ *
+ *   structure 2,3,1
+ *   learning_rate 0.15
+ *   target_total_error 0.0005
+ *   0,0 1
+ *   0,1 1
+ *   1,0 1
+ *   1,1 0
+ */
+TEST_CASE( "reader - integration test - test the dummy file is loaded")
+{
+    Reader reader;
+
+    std::string file_name = "test/training_data";
+
+    TrainingData test_training_data = reader.read_training_data(file_name);
+    
+    std::vector<int> actual_structure = test_training_data.structure;
+    std::vector<int> expected_structure = {2,3,1};
+    REQUIRE(actual_structure == expected_structure); 
+
+    double actual_learning_rate = test_training_data.learning_rate;
+    double expected_learning_rate = 0.15;
+    REQUIRE(actual_learning_rate == expected_learning_rate);
+
+    double actual_target_total_error = test_training_data.target_total_error;
+    double expected_target_total_error = 0.0005;
+    REQUIRE(actual_target_total_error == expected_target_total_error);
+
+    std::vector<double> actual_data = test_training_data.data.at(2);
+    std::vector<double> expected_data = {1.0, 0.0};
+    REQUIRE(actual_data == expected_data);
+
+    std::vector<double> actual_targets = test_training_data.targets.at(3);
+    std::vector<double> expected_targets = {0.0};
+    REQUIRE(actual_targets == expected_targets);
+}
