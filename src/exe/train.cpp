ab4d23203a5e03b4e1b5e7fe4533288180b00235
john smith <jason.robert.thomas.123@gmail.com>
2017-05-17 18:36:04 +1000

    Using reader to parse data file params, taking arguments at terminal

--- src/exe/train.cpp
+++ src/exe/train.cpp
@@ -1,8 +1,13 @@
 #include "../network/network.h"
 #include "../classifier/classifier.h"
 #include "../trainer/trainer.h"
+#include "../reader/reader.h"
+#include "../reader/parsed_data.h"
 #include <vector>
 #include <iostream>
+#include <sstream> //For char conversion
+#include <string> //For char conversion
+
 
 double get_average_error(std::vector<double> all_total_errors)
 {
@@ -15,152 +20,48 @@ double get_average_error(std::vector<double> all_total_errors)
     return average;
 }
 
-
-void all_docs(Network& network, Classifier& classifier, Trainer& trainer)
+void classify_epoch(Network& network, TrainingData& training_data, Classifier& classifier)
 {
+    for(int i=0; i<training_data.data.size(); i++)
+    {
+        std::vector<double> new_inputs = training_data.data.at(i);
+        classifier.set_input_values(new_inputs);
+        classifier.classify();
 
+        for(int j=0; j<new_inputs.size(); j++)
+        {
+            std::cout<< new_inputs.at(j);
+            std::cout<< ",";
+        }
+
+        std::cout<< " - ";
+
+        for(int k=0; k<network.output_layer.size(); k++)
+        {
+            std::cout<< network.output_layer.at(k).outgoing_value;
+            std::cout<< ",";
+        }
+        std::cout<< std::endl;
+    }
+}
+
+void train_epoch(Network& network, TrainingData& training_data, Classifier& classifier, Trainer& trainer)
+{
     std::vector<double> all_total_errors;
 
-    /* Document 1 */
+    for(int i=0; i < training_data.data.size(); i++)
+    {
+        std::vector<double> new_inputs = training_data.data.at(i);
+        classifier.set_input_values(new_inputs);
+        classifier.classify();
 
-    std::vector<double> new_inputs_1;
-    double val_1 = 0.0;
-    double val_2 = 0.0;
-    double val_3 = 0.0;
-    new_inputs_1.emplace_back(val_1);
-    new_inputs_1.emplace_back(val_2);
-    new_inputs_1.emplace_back(val_3);
-    classifier.set_input_values(new_inputs_1);
-    classifier.classify();
-
-    std::vector<double> new_targets_1;
-    double target_1 = 0.0;
-    new_targets_1.emplace_back(target_1);
-    trainer.set_target_values(new_targets_1);
-    trainer.train();
+        std::vector<double> new_targets = training_data.targets.at(i);
+        trainer.set_target_values(new_targets);
+        trainer.train();
  
-    double total_error = trainer.calculate_total_error(network.output_layer);
-    all_total_errors.emplace_back(total_error);
-
-    /* Document 2 */
-    std::vector<double> new_inputs_2;
-    val_1 = 0.0;
-    val_2 = 0.0;
-    val_3 = 1.0;
-    new_inputs_2.emplace_back(val_1);
-    new_inputs_2.emplace_back(val_2);
-    new_inputs_2.emplace_back(val_3);
-    classifier.set_input_values(new_inputs_2);
-    classifier.classify();
-
-    std::vector<double> new_targets_2;
-    target_1 = 0.1;
-    new_targets_2.emplace_back(target_1);
-    trainer.set_target_values(new_targets_2);
-    trainer.train();
-
-    total_error = trainer.calculate_total_error(network.output_layer);
-    all_total_errors.emplace_back(total_error);
-
-    /* Document 3 */
-    std::vector<double> new_inputs_3;
-    val_1 = 0.0;
-    val_2 = 1.0;
-    val_3 = 0.0;
-    new_inputs_3.emplace_back(val_1);
-    new_inputs_3.emplace_back(val_2);
-    new_inputs_3.emplace_back(val_3);
-    classifier.set_input_values(new_inputs_3);
-    classifier.classify();
-
-    std::vector<double> new_targets_3;
-    target_1 = 0.2;
-    new_targets_3.emplace_back(target_1);
-    trainer.set_target_values(new_targets_3);
-    trainer.train();
-
-    total_error = trainer.calculate_total_error(network.output_layer);
-    all_total_errors.emplace_back(total_error);
-
-    /* Document 4 */
-    std::vector<double> new_inputs_4;
-    val_1 = 0.0;
-    val_2 = 1.0;
-    val_3 = 1.0;
-    new_inputs_4.emplace_back(val_1);
-    new_inputs_4.emplace_back(val_2);
-    new_inputs_4.emplace_back(val_3);
-    classifier.set_input_values(new_inputs_4);
-    classifier.classify();
-
-    std::vector<double> new_targets_4;
-    target_1 = 0.3;
-    new_targets_4.emplace_back(target_1);
-    trainer.set_target_values(new_targets_4);
-    trainer.train();
-
-    total_error = trainer.calculate_total_error(network.output_layer);
-    all_total_errors.emplace_back(total_error);
-
-    /* Document 5 */
-    std::vector<double> new_inputs_5;
-    val_1 = 1.0;
-    val_2 = 0.0;
-    val_3 = 0.0;
-    new_inputs_5.emplace_back(val_1);
-    new_inputs_5.emplace_back(val_2);
-    new_inputs_5.emplace_back(val_3);
-    classifier.set_input_values(new_inputs_5);
-    classifier.classify();
-
-    std::vector<double> new_targets_5;
-    target_1 = 0.4;
-    new_targets_5.emplace_back(target_1);
-    trainer.set_target_values(new_targets_5);
-    trainer.train();
-
-    total_error = trainer.calculate_total_error(network.output_layer);
-    all_total_errors.emplace_back(total_error);
-
-    /* Document 6 */
-    std::vector<double> new_inputs_6;
-    val_1 = 1.0;
-    val_2 = 0.0;
-    val_3 = 1.0;
-    new_inputs_6.emplace_back(val_1);
-    new_inputs_6.emplace_back(val_2);
-    new_inputs_6.emplace_back(val_3);
-    classifier.set_input_values(new_inputs_6);
-    classifier.classify();
-
-    std::vector<double> new_targets_6;
-    target_1 = 0.5;
-    new_targets_6.emplace_back(target_1);
-    trainer.set_target_values(new_targets_6);
-    trainer.train();
-
-    total_error = trainer.calculate_total_error(network.output_layer);
-    all_total_errors.emplace_back(total_error);
-
-    /* Document 7 */
-    std::vector<double> new_inputs_7;
-    val_1 = 1.0;
-    val_2 = 1.0;
-    val_3 = 0.0;
-    new_inputs_7.emplace_back(val_1);
-    new_inputs_7.emplace_back(val_2);
-    new_inputs_7.emplace_back(val_3);
-    classifier.set_input_values(new_inputs_7);
-    classifier.classify();
-
-    std::vector<double> new_targets_7;
-    target_1 = 0.6;
-    new_targets_7.emplace_back(target_1);
-    trainer.set_target_values(new_targets_7);
-    trainer.train();
-
-    total_error = trainer.calculate_total_error(network.output_layer);
-    all_total_errors.emplace_back(total_error);
+        double total_error = trainer.calculate_total_error(network.output_layer);
+        all_total_errors.emplace_back(total_error);
+    }
 
     trainer.epoch += 1;
     network.epoch_average_total_error = get_average_error(all_total_errors);
@@ -168,25 +69,32 @@ void all_docs(Network& network, Classifier& classifier, Trainer& trainer)
     {
         std::cout<< trainer.epoch<<", "<< network.epoch_average_total_error<< std::endl;
     }
-
 }
 
-int main()
+int main(int argc, char** argv)
 {
-    Network network(3,30,1);
+    std::string file_name;
+    if(argc > 0)
+    {
+        file_name = std::string(argv[1]);
+    } else
+    {
+        std::string file_name = "data/training/scatter_plot";
+    }
+    Reader reader;
+    TrainingData training_data = reader.read_training_data(file_name);
+    Network network(training_data.structure);
     Classifier classifier(network);
-    double learning_rate = 0.25;
-    Trainer trainer(network, learning_rate);
-
-    double total_target_error = 0.0005;
+    Trainer trainer(network, training_data.learning_rate);
 
     std::cout<< "Epoch"<<", "<< "epoch_average_total_error"<< std::endl;
-    all_docs(network, classifier, trainer);
+    train_epoch(network, training_data, classifier, trainer);
 
-    while(network.epoch_average_total_error > total_target_error)
+    while(network.epoch_average_total_error > training_data.target_total_error)
     {
-        all_docs(network, classifier, trainer);
+        train_epoch(network, training_data, classifier, trainer);
     }
 
+    classify_epoch(network, training_data, classifier);
     return 0;
 }
