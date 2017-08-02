f2f17e58cd03e286629f28c70c9a4194872f3b65
john smith <jason.robert.thomas.123@gmail.com>
2017-08-02 16:30:12 +1000

    Changing header, not passing as reference but using std::move()

--- src/network/neuron/neuron.h
+++ src/network/neuron/neuron.h
@@ -23,7 +23,7 @@ class Neuron
         double error_value;
         std::vector<std::unique_ptr<ISynapse>> incoming_synapses;
         std::vector<std::reference_wrapper<ISynapse>> outgoing_synapses;
-        std::reference_wrapper<ISynapse> add_incoming_synapse(std::unique_ptr<ISynapse>& new_incoming_synapse);
+        std::reference_wrapper<ISynapse> add_incoming_synapse(std::unique_ptr<ISynapse> new_incoming_synapse);
         void add_outgoing_synapse(std::reference_wrapper<ISynapse> new_outgoing_synapse);
 };
 
