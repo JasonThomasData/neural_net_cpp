a7fd7df0a6e52b9516c0e8101aadacb5b4e668e4
john smith <jason.robert.thomas.123@gmail.com>
2017-11-10 23:23:11 +1100

    Another class that can become a namespace

--- src/simple_parser/simple_parser.h
+++ src/simple_parser/simple_parser.h
@@ -1,19 +1,10 @@
-#ifndef SimpleParser_h
-#define SimpleParser_h
+#include <vector>
 
-/* Responsible for parsing raw strings from files into more useful formats.
- */
-
-class SimpleParser
+namespace SimpleParser
 {
-    private:
-        SimpleParser();
-    public:
-        static int to_int(std::string to_convert);
-        static double to_double(std::string to_convert);
-        static std::vector<std::string> string_vector(std::string number_string);
-        static std::vector<int> string_vector_int_vector(std::vector<std::string> parse_string_list);
-        static std::vector<double> string_vector_double_vector(std::vector<std::string> parse_string_list);
+    int to_int(std::string to_convert);
+    double to_double(std::string to_convert);
+    std::vector<std::string> string_vector(std::string number_string);
+    std::vector<int> string_vector_int_vector(std::vector<std::string> parse_string_list);
+    std::vector<double> string_vector_double_vector(std::vector<std::string> parse_string_list);
 };
-
-#endif
